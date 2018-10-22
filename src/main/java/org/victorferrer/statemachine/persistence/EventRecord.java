faf4b4b410fbe81c2ec28518410a8024564fa9c7
EFEVICT <victor.ferrer.posa@ericsson.com>
2018-10-22 09:59:24 +0200

    Fixed Event Record ID Generation strategy to avoid creating the sequence in Postgres

--- src/main/java/org/victorferrer/statemachine/persistence/EventRecord.java
+++ src/main/java/org/victorferrer/statemachine/persistence/EventRecord.java
@@ -18,7 +18,7 @@ public class EventRecord {
 
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     
     private String taskName;
